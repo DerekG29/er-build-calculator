fe0dacd3dcf752ac06669cfd75a50aa99a7751a2
DerekG29 <derekgrubbs29@gmail.com>
2025-07-04 10:33:36 -0400

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import { useState } from 'react'
 import { calc } from './utils';
 import './App.css'
 
+const initialStats = {
+  vig: 10,
+  mnd: 10,
+  end: 10,
+}
+
 function App() {
-  const [vigor, setVigor] = useState(1)
+  const [stats, setStats] = useState(initialStats)
 
   return (
     <>
       <h1>Elden Ring Build Calculator</h1>
       <div className="card">
-        <button onClick={() => setVigor((vigor) => vigor + 1)}>
-          vigor is {vigor}
+        <button onClick={() => setStats({ ...stats, vig: stats.vig + 1 })}>
+          Increase Vigor
         </button>
-        <p>HP is {calc.base_hp(vigor)}</p>
+        <button onClick={() => setStats({ ...stats, vig: stats.vig - 1 })}>
+          Decrease Vigor
+        </button>
+        <p>Vigor: {stats.vig} | HP: {calc.base_hp(stats.vig)}</p>
       </div>
     </>
   )
