f14911618bfefea32538c409a217f433d36be78b
DerekG29 <derekgrubbs29@gmail.com>
2025-07-05 21:44:52 -0400

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,19 @@ function App() {
     <>
       <h1>Elden Ring Build Calculator</h1>
       <div className="card">
-        <button onClick={() => setStats({ ...stats, vig: stats.vig + 1 })}>
-          Increase Vigor
-        </button>
-        <button onClick={() => setStats({ ...stats, vig: stats.vig - 1 })}>
-          Decrease Vigor
-        </button>
-        <p>Vigor: {stats.vig} | HP: {calc.base_hp(stats.vig)}</p>
+        <div className='container'>
+          <div className='row'>
+            <p>Vigor: {stats.vig} | HP: {calc.base_hp(stats.vig)}</p>
+            <button onClick={() => setStats({ ...stats, vig: stats.vig + 1 })}>
+              +
+            </button>
+            <button onClick={() => setStats({ ...stats, vig: stats.vig - 1 })}>
+              -
+            </button>
+          </div>
+          <p>Mind: {stats.mnd} | FP: {calc.base_fp(stats.mnd)}</p>
+          <p>Endurance: {stats.end} | Stamina: {calc.base_fp(stats.end)}</p>
+        </div>       
       </div>
     </>
   )
